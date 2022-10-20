db4bce0e961404242272548486464baac04ec0c1
Alvinjasper <alvinjspr@gmail.com>
2022-10-20 09:40:12 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for checks for a digit 
+ *_isdigit - checks for checks for a digit 
  * @c: c -  Variable
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -9,7 +9,7 @@ int _isdigit(int c)
 {
 	int y = 0;
 
-	if (c >= '48' && c <= '57')
+	if (c >= '0' && c <= '9')
 		y = 1;
 
 	return (y);
