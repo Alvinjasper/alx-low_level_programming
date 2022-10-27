30e577d32825d9932683b9316b3949e2f6589509
Alvinjasper <alvinjspr@gmail.com>
2022-10-27 11:03:55 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @dest: copy to
  * @src: copy from
  * @n: input number of char
- * Return: Always 0 (Success)
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	while (i < n)
 	{
-		dest[i++] = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
