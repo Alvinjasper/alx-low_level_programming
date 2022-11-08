d0c4b5064228494a219ff5982acc12daf2983cd0
Alvinjasper <alvinjspr@gmail.com>
2022-11-08 09:44:53 -0800

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,27 +4,21 @@
 /**
  * _strdup - duplicate to new memory space location
  * @str: char
- * Return: 0
+ * Return: pointer to duplicate string or NULL if failure
  */
 char *_strdup(char *str)
 {
-	int i, end;
-	char *array;
+	char *pstr;
+	unsigned int i = 0;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (end = 0; end <= *str; end++)
-	{
-	}
-
-	end += 1;
-	array = malloc(sizeof(char) * end);
-
-	for (i = 0; i < end; i++)
-		array[i] = str[i];
-
-	if (array == NULL)
-		return (NULL);
-	return (array);
+	if (str == 0)
+		return (0);
+	while (str[i] != 0)
+		i++;
+	i++;
+	pstr = malloc(sizeof(char) * i);
+	for (i = 0; str[i] != 0; i++)
+		pstr[i] = str[i];
+	pstr[i] = 0;
+	return (pstr);
 }
