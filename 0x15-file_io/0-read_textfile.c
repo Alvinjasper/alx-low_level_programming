9e2cb315cd1ed8f2ff376d4a42fa16492f2d42ed
Alvinjasper <alvinjspr@gmail.com>
2022-12-05 07:36:31 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,33 @@
 #include"main.h"
 /**
  * read_textfile - Entry Point
- * @filename: file name
- * @letters: size
- * Return: 0
+ *
+ * @filename: the path of the file
+ * @letters: the number of letters it should read and print
+ *
+ * Return: number of letters it could read and print
+ * or 0 if the file can't be opened, filename is NULL, or write fails
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+ssize_t	read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
-	char *buf;
+	int fd = open(filename, O_RDONLY);
+	ssize_t bytes = 0, write_o = -1;
+	char *buf = NULL;
 
-	if (filename == NULL)
-		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
-		return (0);
-
-	rd = read(file, buf, letters);
-	if (rd == -1)
-		return (0);
-
-	buf[letters] = '\0';
-
-	wr = write(1, buf, rd);
-	if (wr == -1)
-		return (0);
-
-	close(file);
-	free(buf);
-	return (wr);
+	if (fd > -1 && filename)
+	{
+		buf = malloc(sizeof(char) * (letters + 1));
+		if (buf)
+		{
+			bytes = read(fd, buf, letters);
+			buf[bytes] = '\0';
+			if (bytes > -1)
+				write_o = write(STDOUT_FILENO, buf, bytes);
+			free(buf);
+			close(fd);
+			if (write_o > -1)
+				return (bytes);
+		}
+	}
+	return (0);
 }
