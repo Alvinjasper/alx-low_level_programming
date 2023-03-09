c06eedf6b3b7d1080d4990709ff178d54d3c02e9
Alvinjasper <alvinjspr@gmail.com>
2023-03-08 23:46:47 -0800

    fix error

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,22 @@
-#include "main.h"
-
 /**
- * _atoi - converts a string into an interger
- * @s: input
- * Return:  Always 0 (Success)
+ * _atoi - converts a string into an integer
+ * @s: the string to be converted
+ *
+ * Return: the value of the int
  */
-int _atoi(char *s)
+
+int	_atoi(char *s)
 {
-	return (0);
+	int i = 0, sign = 0, res = 0;
+
+	while (s[i] && (!(s[i] >= '0') || !(s[i] <= '9')))
+	{
+		if (s[i] == '-')
+			sign++;
+		i++;
+	}
+	sign = (sign % 2) ? -1 : 1;
+	while (s[i] && ((s[i] >= '0') && (s[i] <= '9')))
+		res = res * 10 + (s[i++] - '0') * sign;
+	return (res);
 }
